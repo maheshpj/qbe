a5aeb9e943f4ddc1505306f5e1064daf62141d3f
Mahesh Jadhav <mahesh430023@gmail.com>
2014-11-06 04:05:04 +0530

    added forms for report for and design view

--- qbeapp/views.py
+++ qbeapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 import datetime
 from sqlalchemy import *
 from sqlalchemy.engine import reflection
+from django import forms
+from django.forms.formsets import formset_factory
 
 engine = create_engine('sqlite:///D:/Mahesh/Projects/qbe/db.sqlite3')
 insp = reflection.Inspector.from_engine(engine)
@@ -37,6 +39,16 @@ def add_design_column(request, table_name, column):
                               {"tables": get_tables(),
                                 "total": get_aggregation(),
                                 "design_fields": design_fields})
+                                
+class QbeForm(forms.Form):
+    report_for = forms.ChoiceField(widget=forms.Select, choices=get_table_names(), required=True, help_text="Report for...")                                
+    designFieldFormSet = formset_factory(DesignField, max_num=20)
     
+class DesignField(forms.Form):
+    show = forms.BooleanField(required=False)
+    sort = forms.BooleanField(required=False)
+    total = forms.ChoiceField(widget=forms.Select, choices=get_aggregation(), required=False)
+    criteria = forms.CharField(max_length=1000)
+    orcriteria = forms.CharField(max_length=1000)
     
         
