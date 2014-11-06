fa5c310e5e7df922dd839918cc6dfbc3e7491493
Mahesh Jadhav <mahesh430023@gmail.com>
2014-11-06 17:37:51 +0530

    add run form changes + temporary avoid CSRF changes for POST

--- qbe/urls.py
+++ qbe/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^$', 'qbeapp.views.index', name='index'),
     url(r'^add/(?P<table_name>\w+)/(?P<column>\w+)/$', 'qbeapp.views.add_design_column', name='adddesign'),    
+    url(r'^report/$', 'qbeapp.views.get_report', name='report'),    
     #url(r'^time/$', 'qbeapp.views.cur_datetime'),
 )
--- qbeapp/views.py
+++ qbeapp/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from qbeapp.dbs import *
+from django.views.decorators.csrf import csrf_exempt
 
 design_fields = []
 
 def index(request):
+    c =  {"tables": get_tables()}
     clear_design_fields()    
-    return render_to_response("qbeapp/index.html", 
-                              {"tables": get_tables()})     
+    return render_to_response("qbeapp/index.html", c)     
     
 def get_aggregation():
     return ['avg', 'count', 'max', 'min', 'sum', 'group by']    
     
 def add_design_column(request, table_name, column):
-    design_fields.append((table_name, column))
+    design_fields.append((table_name, column, None))
     return render_to_response("qbeapp/index.html", 
                               {"tables": get_tables(),
                                 "total": get_aggregation(),
@@ -20,6 +22,17 @@ def add_design_column(request, table_name, column):
                                 
 def clear_design_fields():
     del design_fields[:]
+
+@csrf_exempt   
+def get_report(request):
+    c =  {"tables": get_tables(),
+          "total": get_aggregation(),
+            "design_fields": design_fields}
+    if request.method == 'POST':    
+        print 'getting report...'
+        print request.POST
+        return HttpResponseRedirect("")
+    return render_to_response("qbeapp/index.html", c)    
                                     
 
     
