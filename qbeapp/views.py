cd9d67641d879191f6e94532a1e12606e72f4e7a
Mahesh Jadhav <mahesh430023@gmail.com>
2014-11-07 19:14:00 +0530

    refactor views and read data from forms

--- qbeapp/views.py
+++ qbeapp/views.py
@@ -16,31 +16,36 @@ def index(request, template_name=TEMPLATE_INDEX):
     
     return render_to_response(template_name, c)
 
-def get_design_formset():                
+def get_design_formset():                       
     table_clm_tuples = get_table_clm_tuple()
     DesignFieldFormset = formset_factory(DesignFieldForm, 
                                          extra=len(table_clm_tuples))
-    formset = DesignFieldFormset()
-    count = 0    
-    for form in formset:
-        form.table_name = table_clm_tuples[count][0]
-        form.column_name = table_clm_tuples[count][1]
-        count = count + 1
+    formset = create_formset_from_tables(DesignFieldFormset(), table_clm_tuples)    
     return formset    
-                                
+
+def create_formset_from_tables(formset, tuples):
+    count = 0 
+    for form in formset:
+        form.table_name = tuples[count][0]
+        form.column_name = tuples[count][1]
+        count = count + 1
+    return  formset   
+                            
 def clear_design_fields():
     del design_fields[:]
 
 @csrf_exempt   
 def get_report(request, template_name=TEMPLATE_INDEX):
-    print 'getting report...'
     form = QbeForm(request.POST or None)
     DesignFieldFormset = formset_factory(DesignFieldForm)
-    formset = DesignFieldFormset(request.POST, request.FILES)
-    if form.is_valid():
-        print 'form is valid'
-    if formset.is_valid():
-        print 'formset is valid'
+    formset = DesignFieldFormset(request.POST)
+    if form.is_valid() and formset.is_valid():
+        cd_form = form.cleaned_data
+        #cd_formset = formset.cleaned_data
+        print 'report_for:' + cd_form['report_for']
+        for f in formset.forms:        
+            cd_f = f.cleaned_data   
+            print cd_f
     c =  {"tables": get_tables(), 
           "form": form, 
           "design_fields": formset}    
